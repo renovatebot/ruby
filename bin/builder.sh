1d110cf8b0450ddc33cb263f34e2ac6275f53386
Michael Kriese <michael.kriese@visualon.de>
2021-02-08 20:12:20 +0100

    feat: use builder action

--- bin/builder.sh
+++ bin/builder.sh
@@ -6,8 +6,8 @@ CODENAME=$(. /etc/os-release && echo ${VERSION_CODENAME})
 
 NAME=ruby
 
-echo "Building ruby ${RUBY_VERSION} for ${CODENAME}"
+echo "Building ${NAME} ${RUBY_VERSION} for ${CODENAME}"
 ruby-build ${RUBY_VERSION} /usr/local/${NAME}/${RUBY_VERSION}
 
-echo "Compressing ruby ${RUBY_VERSION} for ${CODENAME}"
+echo "Compressing ${NAME} ${RUBY_VERSION} for ${CODENAME}"
 tar -cJf /cache/${NAME}-${RUBY_VERSION}-${CODENAME}.tar.xz -C /usr/local/${NAME} ${RUBY_VERSION}
